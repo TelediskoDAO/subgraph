c4da12f2a0f9d9d547b911edcfa51ca07cd5176e
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2022-04-03 01:14:36 +0200

    voting/delegation

--- resolution/generated/schema.ts
+++ resolution/generated/schema.ts
@@ -381,6 +381,60 @@ export class Resolution extends Entity {
   }
 }
 
+export class DelegationUser extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("address", Value.fromBytes(Bytes.empty()));
+    this.set("delegated", Value.fromBytes(Bytes.empty()));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save DelegationUser entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        "Cannot save DelegationUser entity with non-string ID. " +
+          'Considering using .toHex() to convert the "id" to a string.'
+      );
+      store.set("DelegationUser", id.toString(), this);
+    }
+  }
+
+  static load(id: string): DelegationUser | null {
+    return changetype<DelegationUser | null>(store.get("DelegationUser", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get address(): Bytes {
+    let value = this.get("address");
+    return value!.toBytes();
+  }
+
+  set address(value: Bytes) {
+    this.set("address", Value.fromBytes(value));
+  }
+
+  get delegated(): Bytes {
+    let value = this.get("delegated");
+    return value!.toBytes();
+  }
+
+  set delegated(value: Bytes) {
+    this.set("delegated", Value.fromBytes(value));
+  }
+}
+
 export class ResolutionManager extends Entity {
   constructor(id: string) {
     super();
--- resolution/src/voting-mapping.ts
+++ resolution/src/voting-mapping.ts
@@ -1,3 +1,21 @@
 import { DelegateChanged } from '../generated/Voting/Voting';
+import { DelegationUser } from '../generated/schema';
+import { log } from '@graphprotocol/graph-ts';
 
-export function handleDelegateChanged(event: DelegateChanged): void {}
+export function handleDelegateChanged(event: DelegateChanged): void {
+  const delegatorHexString = event.params.delegator.toHexString()
+  const newDelegateHexString = event.params.newDelegate.toHexString()
+
+  log.info('VOTING handleDelegateChanged, delegator {}, newDelegate {}', [
+    delegatorHexString,
+    newDelegateHexString
+  ])
+  
+  const currentDelegator = DelegationUser.load(delegatorHexString) || new DelegationUser(delegatorHexString)
+
+  if (currentDelegator) {
+    currentDelegator.address = event.params.delegator
+    currentDelegator.delegated = event.params.newDelegate
+    currentDelegator.save()
+  }
+}
