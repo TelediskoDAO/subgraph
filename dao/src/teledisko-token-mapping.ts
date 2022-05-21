d253b595f2faec92568fd7cb50aba239b1120f18
Stefano Ceschi Berrini <stefano.ceschib@gmail.com>
2022-05-21 22:09:42 +0200

    feat: balances calc

--- dao/src/teledisko-token-mapping.ts
+++ dao/src/teledisko-token-mapping.ts
@@ -1,4 +1,4 @@
-import { BigInt } from '@graphprotocol/graph-ts';
+import { Address } from '@graphprotocol/graph-ts';
 import { Transfer } from "../generated/TelediskoToken/TelediskoToken"
 import { DaoUser } from '../generated/schema';
 import { getDaoManagerEntity } from './dao-manager';
@@ -6,21 +6,28 @@ import { getDaoManagerEntity } from './dao-manager';
 export function handleTransfer(event: Transfer): void {
   const fromHexString = event.params.from.toHexString()
   const toHexString = event.params.to.toHexString()
-  const value = event.params.value.toI32()
+  const value = event.params.value
+  
+  if (event.params.from != Address.zero()) {
+    const daoManagerEntity = getDaoManagerEntity()
+    const fromDaoUser = DaoUser.load(fromHexString) || new DaoUser(fromHexString)
+    if (fromDaoUser) {
+      fromDaoUser.totalBalance = fromDaoUser.totalBalance.minus(value)
+      fromDaoUser.address = event.params.from
+  
+      // if from address is contributor, we should remove value from their unlocked temp balance
+      if (daoManagerEntity.contributorsAddresses.includes(event.params.from)) {
+        fromDaoUser.unlockedTempBalance = fromDaoUser.unlockedTempBalance.minus(value)
+      }
 
-  const daoManagerEntity = getDaoManagerEntity()
-  const fromDaoUser = DaoUser.load(fromHexString) || new DaoUser(fromHexString)
-  if (fromDaoUser) {
-    fromDaoUser.totalBalance = BigInt.fromI32(fromDaoUser.totalBalance.toI32() - value)
-
-    // if from address is contributor, we should remove value from their unlocked temp balance
-    if (daoManagerEntity.contributorsAddresses.includes(event.params.from)) {
-      fromDaoUser.unlockedTempBalance = BigInt.fromI32(fromDaoUser.unlockedTempBalance.toI32() - value)
+      fromDaoUser.save()
     }
   }
 
   const toDaoUser = DaoUser.load(toHexString) || new DaoUser(toHexString)  
   if (toDaoUser) {
-    toDaoUser.totalBalance = BigInt.fromI32(toDaoUser.totalBalance.toI32() + value)
+    toDaoUser.totalBalance = toDaoUser.totalBalance.plus(value)
+    toDaoUser.address = event.params.to
+    toDaoUser.save()
   }
 }
